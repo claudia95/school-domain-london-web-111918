da696598fcd773cff52676743a7e983c9f89c7e3
Claudia Fox <claudiafox95@icloud.com>
2018-11-21 14:43:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,18 @@
 # code here!
 
 class School
-  
-  attr_accessor :name, :grade
+
+attr_accessor :name, :grade
+@@grade_count = 0
   
 def initialize(name, grade)
   @name = name 
   @grade = grade
+  @@grade_count = @@grade_count + 1
+end
+
+def self.grade_count
+  @@grade_count
 end
   
 end
